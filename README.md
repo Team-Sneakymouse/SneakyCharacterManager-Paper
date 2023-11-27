9c00392fbf76d2b400816d41f0639370982b52d5
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-11-27 21:06:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SneakyCharacterManager-Paper
+ Paper plugin for switching characters in DvZ and LoM
---
+++ src/main/java/io/sneakymouse/sneakycharactermanager/SneakyCharacterManager.java
@@ -0,0 +1,12 @@
+package io.sneakymouse.sneakycharactermanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SneakyCharacterManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
