1db6c2af57a3378237472ba40864c680bc2f267d
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-01 23:13:22 +0100

    Added PaperMessagingUtil

--- bungee/src/main/java/net/sneakycharactermanager/bungee/SneakyCharacterManager.java
+++ bungee/src/main/java/net/sneakycharactermanager/bungee/SneakyCharacterManager.java
@@ -5,10 +5,18 @@ import net.sneakycharactermanager.bungee.listeners.PluginMessageListener;
 
 public class SneakyCharacterManager extends Plugin {
 
+    private static SneakyCharacterManager instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
         getProxy().registerChannel("SneakyCharacterManager");
         getProxy().getPluginManager().registerListener(this, new PluginMessageListener());
+    }
+
+    public static SneakyCharacterManager getInstance() {
+        return instance;
     }
 
 }
---
+++ bungee/src/main/java/net/sneakycharactermanager/util/PaperMessagingUtil.java
@@ -0,0 +1,39 @@
+package net.sneakycharactermanager.util;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+import net.md_5.bungee.api.config.ServerInfo;
+import net.sneakycharactermanager.bungee.SneakyCharacterManager;
+
+public class PaperMessagingUtil {
+
+    public void SendPacket(ServerInfo server, String subChannelName, Object... objects) {
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+
+        out.writeUTF( subChannelName );
+
+        for (Object object : objects) {
+            if (object instanceof Boolean o)
+                out.writeBoolean(o);
+            else if (object instanceof Byte o)
+                out.writeByte(o);
+            else if (object instanceof Double o)
+                out.writeDouble(o);
+            else if (object instanceof Float o)
+                out.writeFloat(o);
+            else if (object instanceof Integer o)
+                out.writeInt(o);
+            else if (object instanceof Long o)
+                out.writeLong(o);
+            else if (object instanceof Short o)
+                out.writeShort(o);
+            else if (object instanceof String o)
+                out.writeUTF(o);
+            else SneakyCharacterManager.getInstance().getLogger().severe( "SneakyCharacterManager attempted to write an unidentified object to a ByteArray!" );
+        }
+
+        server.sendData( "SneakyCharacterManager", out.toByteArray() );
+    }
+
+}
