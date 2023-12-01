7d8b06ccf723f7656a9cd5e1ef5b3a57400001ec
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-01 23:17:33 +0100

    Cleanup

--- paper/src/main/java/net/sneakycharactermanager/paper/listeners/BungeeMessageListener.java
+++ paper/src/main/java/net/sneakycharactermanager/paper/listeners/BungeeMessageListener.java
@@ -9,16 +9,16 @@ import com.google.common.io.ByteStreams;
 
 public class BungeeMessageListener implements PluginMessageListener
 {
-
     @Override
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message) {
-        if ( !channel.equalsIgnoreCase( "SneakyCharacterManager" ) )
+        if (!channel.equalsIgnoreCase("SneakyCharacterManager"))
         {
             return;
         }
+
         ByteArrayDataInput in = ByteStreams.newDataInput(message);
         String subChannel = in.readUTF();
-        if ( subChannel.equalsIgnoreCase( "Placeholder" ) )
+        if (subChannel.equalsIgnoreCase("Placeholder"))
         {
 
         }
