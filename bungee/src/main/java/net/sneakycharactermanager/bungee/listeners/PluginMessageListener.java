b3b7fb861ceec2f3500fd33db4ad9d625d640bdb
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-04 20:46:25 +0100

    Removed old notes

--- bungee/src/main/java/net/sneakycharactermanager/bungee/listeners/PluginMessageListener.java
+++ bungee/src/main/java/net/sneakycharactermanager/bungee/listeners/PluginMessageListener.java
@@ -39,20 +39,6 @@ public class PluginMessageListener implements Listener {
                 String playerUUID = in.readUTF();
                 PlayerData playerData = PlayerData.get(playerUUID);
                 playerData.loadLastPlayedCharacter(serverInfo);
-
-                // Get player's bungee character data
-
-                // If bungee character data doesn't exist yet: Make bungee character data, then send packet to backend to make a first character
-
-                // If bungee character data does exist:
-                // Grab UUID of last played character
-                // Grab matching skin and nickname data
-                // Pack these in a standard "load character packet", and send them to the backend
-
-                // In either case: If the backend's "deleteCharacterDataOnServerStart" config is false and the player did not yet have a folder in "characterdata", then the character that they end up on should copy the player's minecraft inventory
-                // This is handled at the backend.
-
-                //PaperMessagingUtil.sendByteArray(serverInfo, "loadCharacter", playerUUID);
                 break;
             default:
                 SneakyCharacterManager.getInstance().getLogger().severe("SneakyCharacterManager received a packet but the subchannel was unknown: " + subChannel);
