fc5d721eaa8550b210a82de7ed68def1dbd8c587
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-13 18:24:23 +0100

    Turns out that it wasn't

--- paper/src/main/java/net/sneakycharactermanager/paper/handlers/skins/SkinQueue.java
+++ paper/src/main/java/net/sneakycharactermanager/paper/handlers/skins/SkinQueue.java
@@ -55,13 +55,10 @@ public class SkinQueue extends BukkitRunnable {
         } else {
             this.busy = true;
             next.convertSkinURL();
-            //TODO: I don't think that this is neccesary
-            Bukkit.getScheduler().runTaskLater(SneakyCharacterManager.getInstance(), () -> {
-                if (next.isProcessed()) {
-                    this.remove(next);
-                }
-                this.busy = false;
-            }, 0);
+            if (next.isProcessed()) {
+                this.remove(next);
+            }
+            this.busy = false;
         }
     }
 }
