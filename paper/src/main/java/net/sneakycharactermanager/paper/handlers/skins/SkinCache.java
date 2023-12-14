d1e9d7f14b7e0a59f3b1c83ecb40c18a0f008be5
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-14 13:21:29 +0100

    Fixed nullpointer

--- paper/src/main/java/net/sneakycharactermanager/paper/handlers/skins/SkinCache.java
+++ paper/src/main/java/net/sneakycharactermanager/paper/handlers/skins/SkinCache.java
@@ -10,17 +10,20 @@ public class SkinCache {
     private static Map<String, Map<String, ProfileProperty>> skinCache = new ConcurrentHashMap<>();
 
     public static ProfileProperty get(String playerUUID, String url) {
-        return skinCache.compute(playerUUID, (key, value) -> {
+        return Optional.ofNullable(skinCache.compute(playerUUID, (key, value) -> {
             if (value != null) {
                 ProfileProperty p = value.get(url);
                 if (p != null && p.isSigned()) {
                     return value;
                 } else {
                     value.remove(url);
+                    if (value.isEmpty()) {
+                        return null;  // Remove the entry if the map is empty
+                    }
                 }
             }
             return null;
-        }).get(url);
+        })).flatMap(map -> Optional.ofNullable(map.get(url))).orElse(null);
     }
 
     public static void put(String playerUUID, String url, ProfileProperty profileProperty) {
