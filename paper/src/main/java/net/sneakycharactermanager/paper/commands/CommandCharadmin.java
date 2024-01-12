fce82e5f3d8d61f703b519e0bb9409fbff60b54e
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-01-12 10:00:25 +0100

    Tabcomplete offline players for charadmin

--- paper/src/main/java/net/sneakycharactermanager/paper/commands/CommandCharadmin.java
+++ paper/src/main/java/net/sneakycharactermanager/paper/commands/CommandCharadmin.java
@@ -59,8 +59,8 @@ public class CommandCharadmin extends Command {
         if (args.length == 1) {
             List<String> playerNames = new ArrayList<>();
 
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (player.getName().toLowerCase().startsWith(args[0].toLowerCase())) playerNames.add(player.getName());
+            for (@NotNull OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+                if (player.getName().toLowerCase().startsWith(args[0].toLowerCase()) && !player.getName().equals("CMI-Fake-Operator")) playerNames.add(player.getName());
             }
 
             return playerNames;
