b4a3f721ec9a02811e61496e346c8838734e4b2b
MomoPewpew <marijntepas@gmail.com>
2025-11-01 08:02:18 +0100

    Moved bungee dependencies to bungee build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
     }
 
     dependencies {
-        compileOnly("net.md-5:bungeecord-api:1.20-R0.1")
+        
     }
 
     java {
--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     id("java")
 }
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // SpigotMC snapshots
+}
+
+dependencies {
+    compileOnly("net.md-5:bungeecord-api:1.20-R0.1") {
+        exclude(group = "net.md-5", module = "brigadier") // Exclude the problematic brigadier dependency
+    }
+}
